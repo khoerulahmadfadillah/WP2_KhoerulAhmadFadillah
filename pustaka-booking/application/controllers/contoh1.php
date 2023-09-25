ef346d9a7cbe139c91df32f719f5552c110ff8ad
khoerulahmadfadillah <khoerulahmadfadillah554@gmail.com>
2023-09-25 17:09:18 +0700

    menambahkan latihan1, mengubah devaulet controler

--- pustaka-booking/application/controllers/contoh1.php
+++ pustaka-booking/application/controllers/contoh1.php
@@ -4,10 +4,10 @@ class contoh1 extends CI_Controller
  public function index()
  {
  echo "<h1>Perkenalkan</h1>";
- echo"Nama saya Imam Nawawi
- Saya tingga di daerah Ciputat
+ echo"Nama saya Khoerul Ahmad Fadillah 
+ Saya tingga di daerah Cikarang 
  olah raga yang saya sukai adalah
- Bulutangkis";
+ gym";
  }
 }
 ?>
---
+++ pustaka-booking/application/controllers/latihan1.php
@@ -0,0 +1,16 @@
+<?php
+class latihan1 extends CI_Controller
+{
+ public function index()
+ {
+ echo "<h1>latihan 1 Perkenalan</h1>";
+ echo "<ul>";
+ echo"<li>Nama  : Khoerul Ahmad Fadillah</li> ";
+ echo"<li>Nim   : 19220756</li>";
+ echo"<li>HP    : 083171868969</li>";
+ echo"<li>Kelas : 19.3A.24 SI.S1</li>";
+ echo"<li>Hobi  : gym</li>";
+ echo "</ul>";
+ }
+}
+?>
