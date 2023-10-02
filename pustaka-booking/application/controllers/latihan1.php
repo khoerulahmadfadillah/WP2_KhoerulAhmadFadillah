f7c3362639158af9c4e9a21f97bd0e711e20d126
khoerulahmadfadillah <khoerulahmadfadillah554@gmail.com>
2023-10-02 17:01:46 +0700

    menghapus index dan mengerjakan contoh5

--- pustaka-booking/application/controllers/latihan1.php
+++ pustaka-booking/application/controllers/latihan1.php
@@ -7,7 +7,7 @@ class latihan1 extends CI_Controller
  echo "<ul>";
  echo"<li>Nama  : Khoerul Ahmad Fadillah</li> ";
  echo"<li>Nim   : 19220756</li>";
- echo"<li>HP    : 083171868969</li>";
+ echo"<li>HP    : 083171868xxx</li>";
  echo"<li>Kelas : 19.3A.24 SI.S1</li>";
  echo"<li>Hobi  : gym</li>";
  echo "</ul>";
