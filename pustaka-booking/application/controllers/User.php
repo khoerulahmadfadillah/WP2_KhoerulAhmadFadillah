4659ff6ce730edbd11e6524311e2d1a123f733e5
khoerulahmadfadillah <khoerulahmadfadillah554@gmail.com>
2023-12-14 16:30:15 +0700

    menyempurnakan pertemuan 9

--- pustaka-booking/application/controllers/User.php
+++ pustaka-booking/application/controllers/User.php
@@ -5,13 +5,13 @@ class User extends CI_Controller
  public function __construct()
  {
  parent::__construct();
- cek_login()
-}
+ cek_login();
+ }
+
 public function index()
 {
 $data['judul'] = 'Profil Saya';
-$data['user'] = $this->ModelUser->cekData(['email' => $this-
->session->userdata('email')])->row_array();
+$data['user'] = $this->ModelUser->cekData(['email' => $this->session->userdata('email')])->row_array();
 $this->load->view('templates/header', $data);
 $this->load->view('templates/sidebar', $data);
 $this->load->view('templates/topbar', $data);
@@ -21,8 +21,7 @@ $this->load->view('templates/footer');
 public function anggota()
 {
 $data['judul'] = 'Data Anggota';
-$data['user'] = $this->ModelUser->cekData(['email' => $this-
->session->userdata('email')])->row_array();
+$data['user'] = $this->ModelUser->cekData(['email' => $this->session->userdata('email')])->row_array();
 $this->db->where('role_id', 1);
 $data['anggota'] = $this->db->get('user')->result_array();
 $this->load->view('templates/header', $data);
@@ -34,8 +33,7 @@ $this->load->view('templates/footer');
 public function ubahProfil()
 {
 $data['judul'] = 'Ubah Profil';
-$data['user'] = $this->ModelUser->cekData(['email' => $this-
->session->userdata('email')])->row_array();
+$data['user'] = $this->ModelUser->cekData(['email' => $this->session->userdata('email')])->row_array();
 $this->form_validation->set_rules('nama', 'Nama Lengkap', 
 'required|trim', [
 'required' => 'Nama tidak Boleh Kosong'
